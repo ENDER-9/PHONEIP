9878c319081c44cae4450794c6f6a23283b68546
ENDER <julian.jaksch@gmx.de>
2020-06-21 18:43:46 +0200

    implemented person class

--- Server/src/database.h
+++ Server/src/database.h
@@ -3,20 +3,6 @@
 #include <sqlite3.h>
 #include <string>
 
-struct Person {
-
-    public:
-    Person (std::string firstName_, std::string lastName_, std::string birthday_, std::string phoneNumber_, std::string street_, std::string city_, std::string postcode_)
-    : firstName (firstName), lastName (lastName_), birthday (birthday_), phoneNumber (phoneNumber_), street (street_), city (city_), postcode (postcode_) {
-    }
-    std::string firstName;
-    std::string lastName;
-    std::string birthday;
-    std::string phoneNumber;
-    std::string street;
-    std::string city;
-    std::string postcode;
-};
 
 class Database {
 
