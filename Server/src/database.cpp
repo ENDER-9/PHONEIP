2505e73b9fea29083226c3cba5b64c7d0ae806eb
ENDER <julian.jaksch@gmx.de>
2020-06-24 12:00:18 +0200

    added methods openDb, closeDb, createTable

--- Server/src/database.cpp
+++ Server/src/database.cpp
@@ -1,1 +1,61 @@
 #include "database.h"
+#include <iostream>
+
+void Database::openDb(const char* fileName) {
+    rc = sqlite3_open(fileName, &db);
+    if(rc != SQLITE_OK) {
+        std::cout << "Error opening database" << sqlite3_errmsg (db) << std::endl;
+    }
+}
+
+void Database::closeDb() {
+    sqlite3_close(db);
+}
+
+void Database::createTable() {
+    openDb(fileName);
+    rc = sqlite3_exec(db,
+                     "CREATE TABLE IF NOT EXISTS Person(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, FirstName TEXT NOT NULL, LastName TEXT NOT NULL, Birthday TEXT NOT NULL, "
+                       "PhoneNumber TEXT NOT NULL, Street TEXT NOT NULL, City TEXT NOT NULL, Postcode INT NOT NULL)",
+                       NULL, 0, NULL);
+    if(rc != SQLITE_OK) {
+        std::cout << "Error creating table" << sqlite3_errmsg(db);
+        closeDb();
+    }
+    closeDb();
+}
+
+std::string Database::addPerson (Person person) {
+
+}
+
+std::string Database::deletePerson (std::string firstName, std::string lastName) {
+
+}
+
+std::string Database::modifyPerson (Person person){
+
+}
+
+//Methods for returning single information
+const std::string Database::getFirstName (std::string firstName, std::string lastName){
+
+}
+const std::string Database::getLastName (std::string firstName, std::string lastName){
+
+}
+const std::string Database::getBirthday (std::string firstName, std::string lastName){
+
+}
+const std::string Database::getPhoneNumber (std::string firstName, std::string lastName){
+
+}
+const std::string Database::getStreet (std::string firstName, std::string lastName){
+
+}
+const std::string Database::getCity (std::string firstName, std::string lastName){
+
+}
+const int Database::getPostcode (std::string firstName, std::string lastName){
+
+}
