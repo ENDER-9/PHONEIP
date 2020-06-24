6ab9a24d0c434647dc5a05b453809f0fe012bc80
ENDER <julian.jaksch@gmx.de>
2020-06-24 12:00:52 +0200

    updated Person class

--- Server/src/person.cpp
+++ Server/src/person.cpp
@@ -1,52 +1,48 @@
 #include "person.h"
 
-Person::Person (std::string firstName_, std::string lastName_, std::string birthday_, std::string phoneNumber_, std::string street_, std::string city_, std::string postcode_);
-    : firstName (firstName), lastName (lastName_), birthday (birthday_), phoneNumber (phoneNumber_), street (street_), city (city_), postcode (postcode_) {
-    }
-    }
 
-    // Methods to get data
-    const std::string& Person::getFirstName () {
-        return firstName;
-    }
-    const std::string& Person::getLastName () {
-        return lastName;
-    }
-    const std::string& Person::getBirthday () {
-        return birthday;
-    }
-    const std::string& Person::getPhoneNumber () {
-        return phoneNumber;
-    }
-    const std::string& Person::getStreet () {
-        return street;
-    }
-    const std::string& Person::getCity () {
-        return city;
-    }
-    const int& Person::getPostcode () {
-        return postcode;
-    }
-    // Methods to change data in Variables
+// Methods to get data
+std::string_view Person::getFirstName () const {
+    return firstName;
+}
+std::string_view Person::getLastName () const {
+    return lastName;
+}
+std::string_view Person::getBirthday () const {
+    return birthday;
+}
+std::string_view Person::getPhoneNumber () const {
+    return phoneNumber;
+}
+std::string_view Person::getStreet () const {
+    return street;
+}
+std::string_view Person::getCity () const {
+    return city;
+}
+int Person::getPostcode () const {
+    return postcode;
+}
+// Methods to change data in Variables
 
-    void Person::setFirstName (std::string& input) {
-        firstName = input;
-    }
-    void Person::setLastName (std::string& input) {
-        lastName = input;
-    }
-    void Person::setBirthday (std::string& input) {
-        birthday = input;
-    }
-    void Person::setPhoneNumber (std::string& input) {
-        phoneNumber = input;
-    }
-    void Person::setStreet (std::string& input) {
-        street = input;
-    }
-    void Person::setCity (std::string& input) {
-        city = input;
-    }
-    void Person::setPostcode (int& input) {
-        postcode = input;
-    }
+void Person::setFirstName (std::string input) {
+    firstName = move (input);
+}
+void Person::setLastName (std::string input) {
+    lastName = move (input);
+}
+void Person::setBirthday (std::string input) {
+    birthday = move (input);
+}
+void Person::setPhoneNumber (std::string input) {
+    phoneNumber = move (input);
+}
+void Person::setStreet (std::string input) {
+    street = move (input);
+}
+void Person::setCity (std::string input) {
+    city = move (input);
+}
+void Person::setPostcode (int input) {
+    postcode = input;
+}
--- Server/src/person.h
+++ Server/src/person.h
@@ -5,26 +5,28 @@
 struct Person {
 
     public:
-    Person (std::string firstName_, std::string lastName_, std::string birthday_, std::string phoneNumber_, std::string street_, std::string city_, std::string postcode_);
+    Person (std::string firstName_, std::string lastName_, std::string birthday_, std::string phoneNumber_, std::string street_, std::string city_, int postcode_)
+    : firstName (firstName), lastName (lastName_), birthday (birthday_), phoneNumber (phoneNumber_), street (street_), city (city_), postcode (postcode_) {
+    }
 
     // Methods to get data
-    const std::string& getFirstName ();
-    const std::string& getLastName ();
-    const std::string& getBirthday ();
-    const std::string& getPhoneNumber ();
-    const std::string& getStreet ();
-    const std::string& getCity ();
-    const int& getPostcode ();
+    std::string_view getFirstName () const;
+    std::string_view getLastName () const;
+    std::string_view getBirthday () const;
+    std::string_view getPhoneNumber () const;
+    std::string_view getStreet () const;
+    std::string_view getCity () const;
+    int getPostcode () const;
 
     // Methods to change data in variables
 
-    void setFirstName (std::string& input);
-    void setLastName (std::string& input);
-    void setBirthday (std::string& input);
-    void setPhoneNumber (std::string& input);
-    void setStreet (std::string& input);
-    void setCity (std::string& input);
-    void setPostcode (int& input);
+    void setFirstName (std::string input);
+    void setLastName (std::string input);
+    void setBirthday (std::string input);
+    void setPhoneNumber (std::string input);
+    void setStreet (std::string input);
+    void setCity (std::string input);
+    void setPostcode (int input);
 
     private:
     std::string firstName;
