c3f04796e5192426953b0330a75070404380db3b
ENDER <julian.jaksch@gmx.de>
2020-06-29 19:07:50 +0200

    implemented recieveInformation method

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -1,11 +1,56 @@
 #include "server.h"
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 void Server::start() {
     boost::asio::ip::tcp::acceptor acceptor (service, boost::asio::ip::tcp::endpoint (boost::asio::ip::tcp::v4 (), port));
-    while(true) {
-        boost::asio::ip::tcp::socket tcpSocket (service);
         std::cout << "waiting for client to connect" << std::endl;
         acceptor.accept (tcpSocket);
+        std::cout << "Client connected to the Server" << std::endl;
+        
+}
+
+void Server::retrieveInformation() {
+    //retrieve Json Object and check which mode is selected
+    std::array<char, 1024> buffer;
+    std::size_t size = tcpSocket.read_some (boost::asio::buffer (buffer, buffer.size ()));
+    std::string message (buffer.begin (), buffer.begin () + size);
+    nlohmann::json personJson = nlohmann::json::parse (message);
+
+    if (personJson["command"] == "addPerson") {
+        std::string firstName = personJson["FirstName"];
+        std::string lastName = personJson["LastName"];
+        std::string birthday = personJson["Birthday"];
+        std::string phoneNumber = personJson["PhoneNumber"];
+        std::string street = personJson["Street"];
+        std::string city = personJson["City"];
+        int postcode = personJson["Postcode"];
+        std::string returnedServerAnswer = database.addPerson (Person (firstName, lastName, birthday, phoneNumber, street, city, postcode));
+        std::cout << "Person added" << std::endl;
     }
+    else if (personJson["command"] == "deletePerson") {
+        std::string firstName = personJson["FirstName"];
+        std::string lastName = personJson["LastName"];
+        std::string returnedServerAnswer = database.deletePerson (firstName, lastName);
+        std::cout << "Person deleted";
+    }
+    else if(personJson["command"] == "modifyPerson") {
+        std::string firstName = personJson["FirstName"];
+        std::string lastName = personJson["LastName"];
+        std::string birthday = personJson["Birthday"];
+        std::string phoneNumber = personJson["PhoneNumber"];
+        std::string street = personJson["Street"];
+        std::string city = personJson["City"];
+        int postcode = personJson["Postcode"];
+        std::string returnedServerAnswer = database.modifyPerson (Person (firstName, lastName, birthday, phoneNumber, street, city, postcode));
+    }
+    else {
+        std::cout << "Nothing found" << std::endl;
+    }
+}
+
+
+void Server::sendInformation(std::string answerToSend) {
+    std::array<char, 1024> buffer;
+    
 }
--- Server/src/server.h
+++ Server/src/server.h
@@ -2,20 +2,27 @@
 #define SERVER_H
 #include <boost/asio.hpp>
 #include "database.h"
+#include "person.h"
 
 class Server {
     public:
-    Server(unsigned short port) : port(port), database("person.db") {
+    Server(unsigned short port) : port(port), tcpSocket(service), database("person.db") {
         database.createTable ();
     }
 
     void start ();
+    void retrieveInformation();
+    void sendInformation (std::string answerToSend);
+
 
 
     private:
     Database database;
+    Person person();
     boost::asio::io_context service;
+    boost::asio::ip::tcp::socket tcpSocket;
     unsigned short port;
+    char modeNumber[1] = {0};
 };
 
 #endif
