8dffa501d7364ffd0636565d3264fa92103fe163
ENDER <julian.jaksch@gmx.de>
2020-07-07 23:11:56 +0200

    Updated main.cpp

--- PhoneBook/src/main.cpp
+++ PhoneBook/src/main.cpp
@@ -3,31 +3,16 @@
 #include <boost/asio.hpp>
 #include <nlohmann/json.hpp>
 #include <iostream>
+#include "client.h"
+#include "frontend.h"
+
+
 
 int main(int argc, char* argv[]) {
 
 
-
-
-    nlohmann::json personJson;
-    personJson["command"] = "deletePerson";
-    personJson["FirstName"] = "Julian";
-    personJson["LastName"] = "Jaksch";
-
-
-
-    boost::asio::io_context service;
-    unsigned short port = 3000;
-    boost::asio::ip::tcp::endpoint server (boost::asio::ip::address::from_string ("127.0.0.1"), port);
-    boost::asio::ip::tcp::socket sock (service);
-    sock.connect (server);
-    boost::asio::write (sock, boost::asio::buffer (personJson.dump (), personJson.dump ().length ()));
-    std::cout << "Message was send!\n";
-
-
-
-
     QCoreApplication::setAttribute (Qt::AA_EnableHighDpiScaling);
+
 
     QGuiApplication app (argc, argv);
 
