97d49b6de0b95e49ecc62f9b1fcd93a2abd43a58
ENDER <julian.jaksch@gmx.de>
2020-07-22 15:21:20 +0200

    Setting up Project for Electron

--- Server/src/database.cpp
+++ Server/src/database.cpp
@@ -1,29 +1,29 @@
 #include "database.h"
 #include <iostream>
 
-void Database::openDb(const char* fileName) {
-    rc = sqlite3_open(fileName, &db);
-    if(rc != SQLITE_OK) {
+void Database::openDb (const char* fileName) {
+    rc = sqlite3_open (fileName, &db);
+    if (rc != SQLITE_OK) {
         std::cerr << "Error opening database" << sqlite3_errmsg (db) << std::endl;
     }
 }
 
-void Database::closeDb() {
-    sqlite3_close(db);
+void Database::closeDb () {
+    sqlite3_close (db);
 }
 
 
-
-void Database::createTable() {
-    openDb(fileName);
-    rc = sqlite3_exec(db,
-                     "CREATE TABLE IF NOT EXISTS Person(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, FirstName TEXT NOT NULL, LastName TEXT NOT NULL, Birthday TEXT NOT NULL, "
+void Database::createTable () {
+    openDb (fileName);
+    rc = sqlite3_exec (db,
+                       "CREATE TABLE IF NOT EXISTS Person(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, FirstName TEXT NOT NULL, LastName TEXT NOT NULL, Birthday TEXT "
+                       "NOT NULL, "
                        "PhoneNumber TEXT NOT NULL, Street TEXT NOT NULL, City TEXT NOT NULL, Postcode INT NOT NULL)",
                        NULL, 0, NULL);
-    if(rc != SQLITE_OK) {
-        std::cerr << "Error creating table" << sqlite3_errmsg(db);
+    if (rc != SQLITE_OK) {
+        std::cerr << "Error creating table" << sqlite3_errmsg (db);
     }
-    closeDb();
+    closeDb ();
 }
 
 std::string Database::addPerson (Person person) {
@@ -34,7 +34,7 @@ std::string Database::addPerson (Person person) {
     if (rc != SQLITE_OK) {
         std::cout << "Error preparing statement: " << sqlite3_errmsg (db);
     }
-    //binding the values to the parameter
+    // binding the values to the parameter
     rc = sqlite3_bind_text (stmt, 1, person.getFirstName ().c_str (), person.getFirstName ().length (), SQLITE_TRANSIENT);
     rc = sqlite3_bind_text (stmt, 2, person.getLastName ().c_str (), person.getLastName ().length (), SQLITE_TRANSIENT);
     rc = sqlite3_bind_text (stmt, 3, person.getBirthday ().c_str (), person.getBirthday ().length (), SQLITE_TRANSIENT);
@@ -43,7 +43,7 @@ std::string Database::addPerson (Person person) {
     rc = sqlite3_bind_text (stmt, 6, person.getCity ().c_str (), person.getCity ().length (), SQLITE_TRANSIENT);
     rc = sqlite3_bind_int (stmt, 7, person.getPostcode ());
 
-    if(rc != SQLITE_OK) {
+    if (rc != SQLITE_OK) {
         std::cerr << "Error bind binding arguments: " << sqlite3_errmsg (db);
     }
     sqlite3_step (stmt);
@@ -54,24 +54,24 @@ std::string Database::addPerson (Person person) {
 }
 
 std::string Database::deletePerson (std::string firstName, std::string lastName) {
-    openDb(fileName);
+    openDb (fileName);
     sqlite3_stmt* stmt;
-    //first check if the Person is in the database
+    // first check if the Person is in the database
     rc = sqlite3_prepare_v2 (db, "SELECT * FROM Person WHERE FirstName = ? AND LastName = ?", -1, &stmt, NULL);
-    if(rc != SQLITE_OK) {
+    if (rc != SQLITE_OK) {
         std::cerr << "Error preparing statement: " << sqlite3_errmsg;
     }
     sqlite3_bind_text (stmt, 1, firstName.c_str (), firstName.length (), NULL);
     sqlite3_bind_text (stmt, 2, lastName.c_str (), lastName.length (), NULL);
-    //delete the Person if it is in the database
-    if(sqlite3_step(stmt) == SQLITE_ROW) {
+    // delete the Person if it is in the database
+    if (sqlite3_step (stmt) == SQLITE_ROW) {
         sqlite3_stmt* deleteStmt;
         rc = sqlite3_prepare_v2 (db, "DELETE FROM Person WHERE FirstName = ? AND LastName = ?", -1, &deleteStmt, NULL);
 
         sqlite3_bind_text (deleteStmt, 1, firstName.c_str (), firstName.length (), SQLITE_TRANSIENT);
         sqlite3_bind_text (deleteStmt, 2, lastName.c_str (), lastName.length (), SQLITE_TRANSIENT);
         sqlite3_step (deleteStmt);
-        sqlite3_finalize(deleteStmt);
+        sqlite3_finalize (deleteStmt);
         closeDb ();
         return "Person was deleted";
     }
@@ -81,7 +81,7 @@ std::string Database::deletePerson (std::string firstName, std::string lastName)
     }
 }
 
-std::string Database::modifyPerson (Person person){
+std::string Database::modifyPerson (Person person) {
     openDb (fileName);
     sqlite3_stmt* stmt;
     // first check if the Person is in the database
@@ -89,14 +89,14 @@ std::string Database::modifyPerson (Person person){
     if (rc != SQLITE_OK) {
         std::cerr << "Error preparing statement: " << sqlite3_errmsg;
     }
-    sqlite3_bind_text (stmt, 1, person.getFirstName().c_str (), person.getFirstName().length (), NULL);
-    sqlite3_bind_text (stmt, 2, person.getLastName().c_str (), person.getLastName().length (), NULL);
+    sqlite3_bind_text (stmt, 1, person.getFirstName ().c_str (), person.getFirstName ().length (), NULL);
+    sqlite3_bind_text (stmt, 2, person.getLastName ().c_str (), person.getLastName ().length (), NULL);
     // modify the Person if it is in the database
     if (sqlite3_step (stmt) == SQLITE_ROW) {
         sqlite3_stmt* modifyStmt;
         rc = sqlite3_prepare_v2 (db, "UPDATE Books SET FirstName = ?, LastName = ?, Birthday = ?, PhoneNumber = ?, Street = ?, City = ?, Postcode = ? WHERE FirstName = ? OR LastName = ?",
-                            -1, &modifyStmt, NULL);
-        if(rc != SQLITE_OK) {
+                                 -1, &modifyStmt, NULL);
+        if (rc != SQLITE_OK) {
             std::cerr << "Error preparing modify statement: " << sqlite3_errmsg (db);
         }
         sqlite3_bind_text (modifyStmt, 1, person.getFirstName ().c_str (), person.getFirstName ().length (), NULL);
@@ -120,8 +120,8 @@ std::string Database::modifyPerson (Person person){
     }
 }
 
-//Methods for returning single information
-const Person Database::getPersonInformation (std::string firstName, std::string lastName){
+// Methods for returning single information
+const Person Database::getPersonInformation (std::string firstName, std::string lastName) {
     openDb (fileName);
     sqlite3_stmt* stmt;
     // first check if the Person is in the database
@@ -160,7 +160,7 @@ const Person Database::getPersonInformation (std::string firstName, std::string 
     }
     else {
         closeDb ();
-        Person errorPerson ("-", "-", "-", "-", "-", "-", 0);
+        Person errorPerson ("Not found", "Not found", "Not found", "Not found", "Not found", "Not found", 0);
         return errorPerson;
     }
 }
